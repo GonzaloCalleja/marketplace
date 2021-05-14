b9090359a28d3ecf229a652760bb48e8f4bdc04f
Gonzalo <gonzacalleja@gmail.com>
2021-05-14 21:32:07 +0200

    Updates

---
+++ precache-manifest.a5d68ef0169fdc5bf2e6572f1782dc22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "badb16832ebe6e5781c0",
+    "url": "/marketplace/static/css/main.83b30e14.chunk.css"
+  },
+  {
+    "revision": "badb16832ebe6e5781c0",
+    "url": "/marketplace/static/js/main.badb1683.chunk.js"
+  },
+  {
+    "revision": "4d34caf5e65add0a6cca",
+    "url": "/marketplace/static/css/1.7e477a14.chunk.css"
+  },
+  {
+    "revision": "4d34caf5e65add0a6cca",
+    "url": "/marketplace/static/js/1.4d34caf5.chunk.js"
+  },
+  {
+    "revision": "dcfa41e5a6248d54ce1d",
+    "url": "/marketplace/static/js/runtime~main.dcfa41e5.js"
+  },
+  {
+    "revision": "0682563374c57f7b251d9ad5ba982327",
+    "url": "/marketplace/static/media/logo.06825633.png"
+  },
+  {
+    "revision": "4ec104ca55fcc35f83254ec61e6ace97",
+    "url": "/marketplace/index.html"
+  }
+];
