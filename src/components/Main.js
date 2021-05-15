a9643d30d1a074873a7ebf256233305c6c87d66f
Gonzalo <gonzacalleja@gmail.com>
2021-05-15 16:25:05 +0200

    remove changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,7 +31,7 @@ class Main extends Component {
               placeholder="Product Price"
               required />
           </div>
-          <button type="submit" className="btn btn-primary">Add Product to Market</button>
+          <button type="submit" className="btn btn-primary">Add Product</button>
         </form>
         <p>&nbsp;</p>
         <h2>Buy Product</h2>
